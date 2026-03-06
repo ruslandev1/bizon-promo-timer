63b9b4a55ca5594b360291735f9195d06728c250
Ruslan Qurbanov <ruslanqurbanovdev@gmail.com>
2026-03-06 10:07:27 +0400

    update: style fixes and font-face scss file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./app').then(m => m.App) },
+  ];
+  
